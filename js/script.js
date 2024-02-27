17803cdb550dbad09952d2de7309ac7cb1a678a4
D_Sykes <Itachi_15_90@yahoo.com>
2024-02-26 22:40:02 -0600

    added JS

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+const learning = {};
+
+learning.topic = "JS";
+learning.learningGoals = ["Learn CSS", "Learn arrays", "Build websites", "Learn Git"];
+learning.category = "Front End Development";
+learning.topicImportance = "High";
+
+console.log(learning);
+
+learning.topic = "Javascript";
+learning.learningGoals.splice(0,1);
+
+console.log(learning);
+
+topicElement.innerText = `I'm learning ${learning.topic}!`;
+topicElement.classList.remove("hide");
+countElement.innerText = `I have ${learning.learningGoals.length} learning goals!`;
+countElement.classList.remove("hide");
